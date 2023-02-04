369fb0bc2f62c63abdab1add8f6d683e465b53c9
Dmitrij <fazzy07@tut.by>
2023-02-04 19:44:34 +0300

    вторая задача

--- Seminar2_Homework/Program.cs
+++ Seminar2_Homework/Program.cs
@@ -10,3 +10,40 @@ int num1 = Convert.ToInt32(Console.ReadLine());
 int num2 = (num1 / 10) % 10;
 Console.WriteLine(num2);
 
+// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+if (numA < 100)
+{
+    Console.WriteLine("Отсутствует третья цифра");
+}
+else if (numA > 99 && numA < 1000)
+{
+    int numB = numA % 10;
+    Console.WriteLine("Третья цифра " + numB);
+}
+else if (numA > 999 && numA < 10000)
+{
+    int numB = (numA / 10) % 10;
+    Console.WriteLine("Третья цифра " + numB);
+}
+else if (numA > 9999 && numA < 100000)
+{
+    int numB = (numA / 100) % 10;
+    Console.WriteLine("Третья цифра " + numB);
+}
+
+
+
+
+// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
