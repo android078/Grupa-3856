a9d97d9f8c9413a07de031551049f9f45b3ce86a
Dmitrij <fazzy07@tut.by>
2023-02-07 00:15:56 +0300

    Задача №23

---
+++ Seminar3_Homework/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+System.Console.WriteLine("Введите число : ");
+int digit1 = int.Parse(Console.ReadLine());
+
+int start1 = 1;
+while(start1 <= digit1)
+{
+    double result3 = Math.Pow(start1, 3 );
+    start1 ++;
+    System.Console.WriteLine(result3 + ", ");
+}
