31f5af9db3103e2cc83a0faa6d4050cfd1f71d22
Dmitrij <fazzy07@tut.by>
2023-02-11 23:55:36 +0300

    Задача №29

--- Seminar4_Homework/Program.cs
+++ Seminar4_Homework/Program.cs
@@ -50,3 +50,22 @@ Console.WriteLine("Сумма всех чисел = " + summ);
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 
 // 6, 1, 33 -> [6, 1, 33]
+
+
+
+int [] numbers = new int[8];
+Console.Write("[");
+
+for (int i = 0; i < numbers.Length; i++)
+ {
+    numbers [i] = new Random().Next(0, 11);
+    Console.Write(" " + Method (i) + " ");
+ }
+Console.Write("]");
+
+int Method (int a)
+{
+    return numbers[a];
+}
+
+
